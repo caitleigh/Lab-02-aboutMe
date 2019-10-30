abd082e9989805328de1c73a448c3c13aec11ac2
‘Cait <caitleigh27@gmail.com>
2019-10-29 21:09:58 -0700

    updated prompts and responses and started to add alerts

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 'use strict';
 
 var visitorName = prompt("What is your name?")
-console.log ('Visitors name' + visitorName)
+console.log ('Visitors name' + visitorName);
 alert('Welcome, ' + visitorName + '!')
 
 var doYouWantToLearn = prompt("Do you want to learn about me?")
 console.log('Do you want to learn about me ' + visitorName + '?');
 if (doYouWantToLearn.toLowerCase () === 'yes') {
-    console.log('Great! Lets go!');
+    /*console.log('Great! Lets go!');*/
+    alert('Great! Lets go!')
 } else {
-    console.log('Too bad... you are going to!');
-};
+    /*console.log('Too bad... you are going to learn today');*/
+    alert('Too bad... you are going to learn today')
+}
 
 var caitsHometown = prompt("Is Cait from Gig Harbor")
 if (caitsHometown.toLowerCase () === 'yes') {
-    console.log('Correct!');
+    /*console.log('Correct!');*/
+    alert('Correct!')
 } else {
-    console.log('Oops, not quite. She IS from Gig Harbor');
+    /*console.log('Oops, not quite. She IS from Gig Harbor');*/
+    alert('Oops, not quite. She IS from Gig Harbor')
 }
 
 var catName = prompt("Is Cait's cats name Bruce?")
 if (catName.toLowerCase () === 'yes') {
-    console.log ('Yes! His name is burce and he is great!');
+    /*console.log ('Yes! His name is burce and he is great!');*/
+    alert ('Yes! His name is burce and he is great!')
 } else {
-    console.log ('WRONG! His name is Bruce and he is amazing!');
+    /*console.log ('WRONG! His name is Bruce and he is amazing!');*/
+    alert('WRONG! His name is Bruce and he is amazing!')
+}
+
+var college = prompt ("Did Cait go to Idaho State?")
+if (college.toLowerCase () === 'no') {
+    console.log('Yes ' + visitorName + '. You are so smart!');
+} else {
+    console.log ('Common ' + visitorName + '! You should know this! U of I is where it is at!');
+}
+
+var collegeSport = prompt ("Did Cait play soccer in college?")
+if (collegeSport.toLowerCase () === 'no') {
+    console.log ('Not quite! She ran track in college');
+} else {
+    console.log ('You are right! She ran track!');
+}
+
+var favHobbie = prompt ("Does Cait hate boating?")
+if (favHobbie.toLowerCase () === 'no') {
+    console.log ('So so wrong! She loves boating');
+} else {
+    console.log ('Correct! She is out on the water all summer');
 }
