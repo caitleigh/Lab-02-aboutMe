2c2fc54ee2127b235f12e0d516113c66209f6e8e
‘Cait <caitleigh27@gmail.com>
2019-10-31 20:36:23 -0700

    Added top 10 to HTML and added a 6th question with 4 attempts to answer, started adding a second question

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //Welcome message//
 var visitorName = prompt('What is your name?');
 console.log ('Visitors name' + visitorName);
@@ -46,7 +47,7 @@ if (college === 'no' || college === 'n') {
   alert('Common ' + visitorName + '! You should know this! U of I is where it is at!');
 }
 
-//fourth question// 
+//fourth question//
 var collegeSport = prompt ('Did Cait play soccer in college?').toLowerCase;
 if (collegeSport === 'no' || collegeSport === 'n') {
   /*console.log ('You are right + visitorName! She ran track!');*/
@@ -66,5 +67,44 @@ if (favHobbie === 'no' || favHobbie === 'n') {
   alert('So so wrong! She loves boating!');
 }
 
+//Sixth question//
+
+var maxCount = 5;
+var number = parseInt(prompt ('What year did Cait graduate from U of I'));
+while (maxCount > 0) {
+  if (maxCount === 1) {
+    alert('The correct answer is 2014');
+    break;
+  } else if (number > 2014) {
+    alert(number + ' is too high');
+    number = parseInt(prompt('Guess again'));
+    maxCount--;
+  } else if (number < 2014) {
+    alert(number + ' is too low');
+    number = parseInt(prompt('Guess again'));
+    maxCount--;
+  } else if (number === 2014) {
+    alert(number + ' is correct');
+    break;
+  }
+}
+
+//7th question //
+
+var maxAnswers = 6;
+var topSongs = ['Feel It', 'Nerd', 'Always Waiting', 'High Beams', 'Oops I did It Again', 'Water', 'Lucid'];
+while (maxAnswers > 0) {
+  if (maxAnswers === 1) {
+    alert([] + ' is correct!');
+    break;
+  } if else (maxAnswers > 0) {
+    alert('Wrong... guess again');
+  } else (maxAnswers === [0]) {
+    alert
+  }
+  break;
+}
+
 //final message//
-alert ('Thanks for playing! Cait sounds pretty cool, right ' + visitorName + '?');
+alert('Thanks for playing! Cait sounds pretty cool, right ' + visitorName + '?');
+
