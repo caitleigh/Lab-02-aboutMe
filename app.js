733547d285407c19cc056448f555a6764c6b5750
‘Cait <caitleigh27@gmail.com>
2019-11-02 13:48:13 -0700

    updated question 7 code to show only correct answers in array and added a while loop. Updated if else statement

--- app.js
+++ app.js
@@ -6,109 +6,113 @@ var visitorName = prompt('What is your name?');
 console.log ('Visitors name' + visitorName);
 alert('Welcome, ' + visitorName + '!');
 
-//Asking if they would like to learn about me//
-var doYouWantToLearn = prompt('Do you want to learn about me?').toLowerCase;
-console.log('Do you want to learn about me ' + visitorName + '?');
-if (doYouWantToLearn === 'yes' || doYouWantToLearn === 'y') {
-  /*console.log('Great! Lets go!');*/
-  alert('Great! Lets go!');
-} else {
-  /*console.log('Too bad... you are going to learn today');*/
-  alert('Too bad... you are going to learn today');
-}
+// //Asking if they would like to learn about me//
+// var doYouWantToLearn = prompt('Do you want to learn about me?').toLowerCase;
+// console.log('Do you want to learn about me ' + visitorName + '?');
+// if (doYouWantToLearn === 'yes' || doYouWantToLearn === 'y') {
+//   /*console.log('Great! Lets go!');*/
+//   alert('Great! Lets go!');
+// } else {
+//   /*console.log('Too bad... you are going to learn today');*/
+//   alert('Too bad... you are going to learn today');
+// }
 
-//first question//
-var caitsHometown = prompt('Is Cait from Gig Harbor').toLowerCase;
-if (caitsHometown === 'yes' || caitsHometown === 'y') {
-  /*console.log('Correct!');*/
-  alert('Correct!');
-} else {
-  /*console.log('Oops, not quite. She IS from Gig Harbor');*/
-  alert('Oops, not quite. She IS from Gig Harbor');
-}
+// //first question//
+// var caitsHometown = prompt('Is Cait from Gig Harbor').toLowerCase;
+// if (caitsHometown === 'yes' || caitsHometown === 'y') {
+//   /*console.log('Correct!');*/
+//   alert('Correct!');
+// } else {
+//   /*console.log('Oops, not quite. She IS from Gig Harbor');*/
+//   alert('Oops, not quite. She IS from Gig Harbor');
+// }
 
-//second question//
-var catName = prompt('Is Cait\'s cats name Bruce?').toLowerCase;
-if (catName === 'yes' || catName === 'y') {
-  /*console.log ('Yes! His name is Bruce and he is great!');*/
-  alert('Yes! His name is Bruce and he is great!');
-} else {
-  /*console.log ('WRONG! His name is Bruce and he is amazing!');*/
-  alert('WRONG! His name is Bruce and he is amazing!');
-}
+// //second question//
+// var catName = prompt('Is Cait\'s cats name Bruce?').toLowerCase;
+// if (catName === 'yes' || catName === 'y') {
+//   /*console.log ('Yes! His name is Bruce and he is great!');*/
+//   alert('Yes! His name is Bruce and he is great!');
+// } else {
+//   /*console.log ('WRONG! His name is Bruce and he is amazing!');*/
+//   alert('WRONG! His name is Bruce and he is amazing!');
+// }
 
-//third qeustion//
-var college = prompt ('Did Cait go to Idaho State?').toLowerCase;
-if (college === 'no' || college === 'n') {
-  /* console.log('Yes ' + visitorName + '. You are so smart!');*/
-  alert('Yes ' + visitorName + '. You are so smart!');
-} else {
-  /*console.log ('Common ' + visitorName + '! You should know this! U of I is where it is at!');*/
-  alert('Common ' + visitorName + '! You should know this! U of I is where it is at!');
-}
+// //third qeustion//
+// var college = prompt ('Did Cait go to Idaho State?').toLowerCase;
+// if (college === 'no' || college === 'n') {
+//   /* console.log('Yes ' + visitorName + '. You are so smart!');*/
+//   alert('Yes ' + visitorName + '. You are so smart!');
+// } else {
+//   /*console.log ('Common ' + visitorName + '! You should know this! U of I is where it is at!');*/
+//   alert('Common ' + visitorName + '! You should know this! U of I is where it is at!');
+// }
 
-//fourth question//
-var collegeSport = prompt ('Did Cait play soccer in college?').toLowerCase;
-if (collegeSport === 'no' || collegeSport === 'n') {
-  /*console.log ('You are right + visitorName! She ran track!');*/
-  alert('You are right ' + visitorName + '! She ran track!');
-} else {
-  /*console.log ('Not quite! She ran track in college')*/
-  alert('Not quite! She ran track in college');
-}
+// //fourth question//
+// var collegeSport = prompt ('Did Cait play soccer in college?').toLowerCase;
+// if (collegeSport === 'no' || collegeSport === 'n') {
+//   /*console.log ('You are right + visitorName! She ran track!');*/
+//   alert('You are right ' + visitorName + '! She ran track!');
+// } else {
+//   /*console.log ('Not quite! She ran track in college')*/
+//   alert('Not quite! She ran track in college');
+// }
 
-//fifth question //
-var favHobbie = prompt ('Does Cait hate boating?').toLowerCase;
-if (favHobbie === 'no' || favHobbie === 'n') {
-  /*console.log ('Correct! She is out on the water all summer long') */
-  alert('Correct! She is out on the water all summer long');
-} else {
-  /*console.log ('So so wrong! She loves boating!') */
-  alert('So so wrong! She loves boating!');
-}
+// //fifth question //
+// var favHobbie = prompt ('Does Cait hate boating?').toLowerCase;
+// if (favHobbie === 'no' || favHobbie === 'n') {
+//   /*console.log ('Correct! She is out on the water all summer long') */
+//   alert('Correct! She is out on the water all summer long');
+// } else {
+//   /*console.log ('So so wrong! She loves boating!') */
+//   alert('So so wrong! She loves boating!');
+// }
 
-//Sixth question//
+// //Sixth question//
 
-var maxCount = 5;
-var number = parseInt(prompt ('What year did Cait graduate from U of I'));
-while (maxCount > 0) {
-  if (maxCount === 1) {
-    alert('The correct answer is 2014');
-    break;
-  } else if (number > 2014) {
-    alert(number + ' is too high');
-    number = parseInt(prompt('Guess again'));
-    maxCount--;
-  } else if (number < 2014) {
-    alert(number + ' is too low');
-    number = parseInt(prompt('Guess again'));
-    maxCount--;
-  } else if (number === 2014) {
-    alert(number + ' is correct');
-    break;
-  }
-}
+// var maxCount = 5;
+// var number = parseInt(prompt ('What year did Cait graduate from U of I'));
+// while (maxCount > 0) {
+//   if (maxCount === 1) {
+//     alert('The correct answer is 2014');
+//     break;
+//   } else if (number > 2014) {
+//     alert(number + ' is too high');
+//     number = parseInt(prompt('Guess again'));
+//     maxCount--;
+//   } else if (number < 2014) {
+//     alert(number + ' is too low');
+//     number = parseInt(prompt('Guess again'));
+//     maxCount--;
+//   } else if (number === 2014) {
+//     alert(number + ' is correct');
+//     break;
+//   }
+// }
 
 //7th question //
 
-var topSongs = prompt ('What are Caits top songs on Spotify?').toLowerCase;
-var listOfSongs = ['feel it', 'nerd', 'always waiting', 'high beams', 'oops i did it again', 'water', 'lucid'];
-for (var i = 0; i < listOfSongs.length ; i++) {
-  if (topSongs === listOfSongs.indexOf (0, 2, 3)) {
-    alert('That is correct!');
-    break;
-  } else if (topSongs === listOfSongs.indexOf (2)) {
-    alert ('You are right!');
-    break;
-  } else if (topSongs === listOfSongs.indexOf (3)) {
-    alert ('Spot on!');
-    break;
-  } else {
-    alert('Not quite. Guess again!');
+var songArray = ['feel it', 'always waiting', 'high beams'];
 
+var topSongs = prompt ('What is one of Caits top songs on Spotify? You have 6 attempts to guess the correct answer.').toLowerCase;
+
+var songGuesses = 6;
+
+while (songGuesses > 0) {
+  for (var i = 0; i <songArray.length; i++) {
+    if (songArray === topSongs[i])
+      alert('You are correct!' + visitorName);
+    songGuesses = 0;
   }
 }
-
+songGuesses --;
+if (songGuesses !== 0 && songGuesses !== -1) {
+  alert('
+} else if (songGuesses !== 0) {
+  alert('Not quite.');
+  topSongs = prompt('Guess again!');
+} else if (songGuesses === -1) {
+  alert('The correct answers were Feel It, Always Waiting, and High Beam');
+}
 
 //final message//
 alert('Thanks for playing! Cait sounds pretty cool, right ' + visitorName + '?');
